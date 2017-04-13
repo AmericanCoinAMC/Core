8e8d4c0117e7df6476fdbb205ba572c19a672ac8
JessDotJS <jessmanny19@gmail.com>
2017-04-13 11:19:47 -0400

    American coin new conf

---
+++ src/P2p/P2pNetworks.h
@@ -0,0 +1,10 @@
+// Copyright (c) 2011-2016 The Cryptonote developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#pragma once
+
+namespace CryptoNote
+{
+  const static boost::uuids::uuid CRYPTONOTE_NETWORK = { { 0x53, 0x2A, 0x88, 0x5A, 0xA4, 0xff, 0x1e, 0xfe, 0x10, 0xA5, 0x5A, 0xef, 0xAe, 0xee, 0x1A, 0xA1 } };
+}
