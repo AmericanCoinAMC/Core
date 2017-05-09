89ff8eee2c73e1101c17cc27b34438252697bec0
JessDotJS <jessmanny19@gmail.com>
2017-05-09 19:08:12 -0400

    Cache Clean

--- build/release/external/miniupnpc/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/jessdotjs/Projects/AmericanCoin/core/external/miniupnpc
-# Build directory: /home/jessdotjs/Projects/AmericanCoin/core/build/release/external/miniupnpc
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
